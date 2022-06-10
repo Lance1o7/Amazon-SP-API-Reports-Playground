94c31873a16db75d53cb5f5c6400a446f4115baf
Lance <10292888+LanceSuen@users.noreply.github.com>
2022-06-10 00:16:14 -0700

    Fix the path

--- Service.py
+++ Service.py
@@ -123,6 +123,6 @@ def downloadReportFromInput(form):
         )
         res = Reports(credentials=credentials).get_report_document(documentID, decrypte=True, download=True, file=f)
         #print(res)
-        return filename+".json"
+        return path
     except:
         return "Something wrong. Please check your input."
